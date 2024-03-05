22fcb49d64e71cd6d664d38e327575850610cafe
chihatw <chiha.tw@gmail.com>
2024-03-05 15:30:57 +0900

    dummy

--- app/raw/page.tsx
+++ app/raw/page.tsx
@@ -1,7 +1,7 @@
 type Props = {};
 
 const RawPage = (props: Props) => {
-  return <div className='max-w-lg mx-auto pt-10'>RawPage</div>;
+  return <div className='max-w-lg mx-auto pt-10'>RawPage dummy</div>;
 };
 
 export default RawPage;
